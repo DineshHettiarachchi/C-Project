c3e922c4639e6dc107e48c1c1ae6dbff5dde220d
DineshHettiarachchi <40357704+DineshHettiarachchi@users.noreply.github.com>
2018-06-19 17:19:31 +0530

    Add files via upload

---
+++ Uni.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() { printf("\nUniversity of Moratuwa,\n");
+             printf("Katubedda,\n");
+	     printf("Moratuwa,\n");
+	     printf("Sri Lanka\n");
+	     printf("--------------------\n");
+	     printf("www.mrt.ac.lk\n");
+	     return 0;
+}
